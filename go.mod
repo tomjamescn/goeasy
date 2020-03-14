96e0e304c4aefeb29796b711612484ef013cbbb9
tomjamescn <tomjamescn@gmail.com>
2020-03-14 08:48:20 +0800

    string/substr

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tomjamescn/goeasy
+
+go 1.13
---
+++ string/substr.go
@@ -0,0 +1,27 @@
+package string
+
+func Head(str string, n int) string {
+	runes := []rune(str)
+	if n == 0 {
+		return ""
+	} else if n < 0 {
+		return Tail(str, -n)
+	} else if n >= len(runes) {
+		return str
+	} else {
+		return string(runes[:n-1])
+	}
+}
+
+func Tail(str string, n int) string {
+	runes := []rune(str)
+	if n == 0 {
+		return ""
+	} else if n < 0 {
+		return Head(str, -n)
+	} else if n >= len(runes) {
+		return str
+	} else {
+		return string(runes[n-1:])
+	}
+}
